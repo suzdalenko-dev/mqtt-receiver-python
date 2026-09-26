2c271a4097b0af39a80c3e49b320bebd62147184
suzdalenko-dev <alexey.suzdalenko@gmail.com>
2026-09-26 16:31:40 +0200

    suzdalenko

--- receiver-app/sfunctions/db_writer.py
+++ receiver-app/sfunctions/db_writer.py
@@ -3,6 +3,11 @@ import queue, json, threading
 DB_QUEUE      = queue.Queue(maxsize=11111)
 
 
+def insert_date_to_db(m):
+    pass
+
+
+
 def put_message_to_db_queue(date_utc, date_local, topic, content):
     global DB_QUEUE
     date_utc   = str(date_utc)[:23]
@@ -17,7 +22,25 @@ def put_message_to_db_queue(date_utc, date_local, topic, content):
         DB_QUEUE.put_nowait(message)
     except Exception as e:
         print(f"Error insert data to db queue {e}")
-    
+
+
+
+def db_writer():
+    global DB_QUEUE
+    while True:
+        try:
+            m = DB_QUEUE.get()
+            insert_date_to_db(m)
+        except Exception as e:
+            print(f"Error insert data to DB {e}")
+        finally:
+            DB_QUEUE.task_done()
+             
+     
 
 def start_db_inserter():
-    tread = threading.Thread()
+    thread = threading.Thread(
+    target=db_writer,
+    name="postgres-db-writer",
+    daemon=True,
+)
